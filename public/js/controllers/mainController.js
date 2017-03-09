107ae91344927f733b9e403ecce0b190d75e9423
assafzarfaty <zarfaty@blipitapp.com>
2017-03-09 18:10:11 +0200

    extension 1

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,17 +1,26 @@
 app.controller('beersCtrl',function ($scope , beersService ){
   $scope.beers = beersService.beerList;
+  $scope.firstRate = 0;
+   $scope.secondRate = 0;
+   $scope.readOnly = true;
 
   $scope.delete = function(beerIndex){
     $scope.beers.splice(beerIndex,1);
   };
 
   $scope.addBeer = function(){
-    alert("hey");
     $scope.beers.push({
       name : $scope.name,
       style : $scope.style,
       abv : $scope.abv,
-      image : $scope.image
+      image : $scope.image,
+      rating : $scope.secondRate
     });
+    console.log($scope.beers);
   };
+
+   $scope.onItemRating = function(rating , beerIndex){
+     $scope.secondRate = rating;
+   };
+
 });
--- public/js/services/beers.js
+++ public/js/services/beers.js
@@ -1,7 +1,7 @@
 app.factory('beersService', function(){
 var beerList = [
-  {name : "beerExample", style : "cool Style", abv : "12%", image : "http://i138.photobucket.com/albums/q248/mattsniz/Islamorada%202009/Coronas.jpg"},
-  {name : "beerExample", style : "cool Style", abv : "12%", image : "http://i138.photobucket.com/albums/q248/mattsniz/Islamorada%202009/Coronas.jpg"}
+  {name : "Corona", style : "Pale Ale", abv : "6%", image : "http://i138.photobucket.com/albums/q248/mattsniz/Islamorada%202009/Coronas.jpg", rating : {counter:3,totalRanking:13,avgRank: function(){return this.totalRanking/this.counter} }},
+  {name : "Corona", style : "Pale Ale", abv : "6%", image : "http://i138.photobucket.com/albums/q248/mattsniz/Islamorada%202009/Coronas.jpg", rating : {counter:2,totalRanking:8,avgRank: function(){return this.totalRanking/this.counter} }}
 ];
   return {
     beerList : beerList
