169b4bf5fbd6160d4cb771065e65975db2fb6437
assafzarfaty <zarfaty@blipitapp.com>
2017-03-09 18:54:41 +0200

    rating

--- public/js/services/beers.js
+++ public/js/services/beers.js
@@ -1,8 +1,9 @@
 app.factory('beersService', function(){
 var beerList = [
-  {name : "Corona", style : "Pale Ale", abv : "6%", image : "http://i138.photobucket.com/albums/q248/mattsniz/Islamorada%202009/Coronas.jpg", rating : {counter:3,totalRanking:13,avgRank: function(){return this.totalRanking/this.counter} }},
-  {name : "Corona", style : "Pale Ale", abv : "6%", image : "http://i138.photobucket.com/albums/q248/mattsniz/Islamorada%202009/Coronas.jpg", rating : {counter:2,totalRanking:8,avgRank: function(){return this.totalRanking/this.counter} }}
+  {name : "Corona", style : "Pale Ale", abv : "6%", image : "http://i138.photobucket.com/albums/q248/mattsniz/Islamorada%202009/Coronas.jpg", rating : {counter:3,totalRanking:13, avg : 0}},
+  {name : "Corona", style : "Pale Ale", abv : "6%", image : "http://i138.photobucket.com/albums/q248/mattsniz/Islamorada%202009/Coronas.jpg", rating : {counter:2,totalRanking:8, avg: 0}}
 ];
+
   return {
     beerList : beerList
   };
