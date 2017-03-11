04197d8ee09de60b31278f0052f64ad51d7ac431
assafzarfaty <zarfaty@blipitapp.com>
2017-03-11 16:35:19 +0200

    crud features

---
+++ BeerModel.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//Generate new Schema object
+var beerSchema = new Schema({
+  name: String,
+  abv: Number,
+  style: String,
+  image : String,
+  rating : {
+    counter : Number,
+    totalRanking : Number,
+    avg : Number
+  }
+});
+
+//creating a model of a schema object personSchema
+var Beer = mongoose.model('Beer', beerSchema);
+//in order to user Beer model in other files we need to export this 
+module.exports = Beer;
